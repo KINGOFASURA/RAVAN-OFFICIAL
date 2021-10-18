f76b8d34202a356ff8a19aef5ea4fdd9e4a545fc
KINGOFASURA <92657138+KINGOFASURA@users.noreply.github.com>
2021-10-18 05:44:54 +0530

    Update mongo.py

--- YoneRobot/mongo.py
+++ YoneRobot/mongo.py
@@ -1,24 +1,24 @@
-#Kiitu
+#sahil
 import asyncio
 import sys
 
 # from motor import motor_asyncio
-from YoneRobot import MONGO_DB_URI 
+from RAVANOFFICIAL import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from YoneRobot.conf import get_int_key, get_str_key
+from RAVANOFFICIAL.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "YoneX"
+MONGO_DB = "RAVANOFFICIAL"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 # motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 # db = motor[MONGO_DB]
-db = client["yonerobot"]
+db = client["RAVANOFFICIAL"]
 # try:
 #     asyncio.get_event_loop().run_until_complete(motor.server_info())
 # except ServerSelectionTimeoutError:
