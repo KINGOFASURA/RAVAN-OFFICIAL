a9665b22d64024e66cb17612da8b0f82b3496bae
RAVANOFFICIAL <92657138+KINGOFASURA@users.noreply.github.com>
2021-10-19 13:57:16 +0530

    Update __init__.py

--- RAVAN-OFFICIAL/__init__.py
+++ RAVAN-OFFICIAL/__init__.py
@@ -267,7 +267,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from AsunaRobot.modules.helper_funcs.handlers import (
+from RAVANOFFICIAL.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
