b0e8d1453f9cd4ee2e1b46ef43a0f51e8c044226
RAVANOFFICIAL <92657138+KINGOFASURA@users.noreply.github.com>
2021-10-19 15:42:12 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE RAVANOFICIAL
+TITLE RAVAN-OFFICIAL
 :: Enables virtual env mode and then starts Rika
-env\scripts\activate.bat && py -m YoneRobot
+env\scripts\activate.bat && py -m RAVAN-OFFICIAL
