0796dc4c135d0bd17a84dee44854ca239b69487f
RAVANOFFICIAL <92657138+KINGOFASURA@users.noreply.github.com>
2021-10-19 14:03:22 +0530

    Update sample_config.py

--- RAVAN-OFFICIAL/sample_config.py
+++ RAVAN-OFFICIAL/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/AsunaRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/RAVANOFFICIAL/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -18,13 +18,13 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "The_Ghost_Hunter"
-    SUPPORT_CHAT = "AsunaRobotSupport"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "D3VILRAVANXOP"
+    SUPPORT_CHAT = "Cutie_robot_hai"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001432609692
+        -1001690719442
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001432768300
+        -1001690719442
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
